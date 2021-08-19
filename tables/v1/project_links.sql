ae51ab4f8026775190a7991572793d63bb543b7c
Gavin M. Roy <gavinr@aweber.com>
2021-08-19 11:08:29 -0400

    Is for the link type, not the link itself

--- tables/v1/project_links.sql
+++ tables/v1/project_links.sql
@@ -1,14 +1,13 @@
 SET search_path=v1, public, pg_catalog;
 
 CREATE TABLE IF NOT EXISTS project_links (
-  project_id             INTEGER                   NOT NULL,
-  link_type_id           INTEGER                   NOT NULL,
-  created_at             TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-  created_by             TEXT                      NOT NULL,
-  last_modified_at       TIMESTAMP WITH TIME ZONE,
-  last_modified_by       TEXT,
-  url                    TEXT                      NOT NULL,
-  is_primary_repository  BOOLEAN                 NOT NULL  DEFAULT FALSE,
+  project_id        INTEGER                   NOT NULL,
+  link_type_id      INTEGER                   NOT NULL,
+  created_at        TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+  created_by        TEXT                      NOT NULL,
+  last_modified_at  TIMESTAMP WITH TIME ZONE,
+  last_modified_by  TEXT,
+  url               TEXT                      NOT NULL,
   PRIMARY KEY (project_id, link_type_id),
   FOREIGN KEY (project_id) REFERENCES projects (id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (link_type_id) REFERENCES project_link_types (id) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -22,7 +21,6 @@ COMMENT ON COLUMN project_links.created_by IS 'The user created the record';
 COMMENT ON COLUMN project_links.last_modified_at IS 'When the record was last modified';
 COMMENT ON COLUMN project_links.last_modified_by IS 'The user that last modified the record';
 COMMENT ON COLUMN project_links.url IS 'The URL of the link';
-COMMENT ON COLUMN project_links.is_primary_repository IS 'Is this the primary source-code repository link?';
 
 GRANT SELECT ON project_links TO reader;
 GRANT SELECT, INSERT, UPDATE, DELETE ON project_links TO writer;
