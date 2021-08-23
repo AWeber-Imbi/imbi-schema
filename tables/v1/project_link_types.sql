bab8d02570bbf6f54c6affb9699352a10f723504
Gavin M. Roy <gavinr@aweber.com>
2021-08-23 13:38:34 -0400

    Add the created_by_automation field

--- tables/v1/project_link_types.sql
+++ tables/v1/project_link_types.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS project_link_types (
   last_modified_by       TEXT,
   link_type              TEXT                      NOT NULL  UNIQUE,
   icon_class             TEXT                                DEFAULT 'fas fa-link',
-  is_primary_repository  BOOLEAN                   NOT NULL  DEFAULT FALSE
+  is_primary_repository  BOOLEAN                   NOT NULL  DEFAULT FALSE,
+  created_by_automation  BOOLEAN                   NOT NULL  DEFAULT FALSE
 );
 
 COMMENT ON TABLE project_link_types IS 'Table of the types of links allowed for a project';
@@ -20,6 +21,7 @@ COMMENT ON COLUMN project_link_types.last_modified_by IS 'The user that last mod
 COMMENT ON COLUMN project_link_types.link_type IS 'The project link type';
 COMMENT ON COLUMN project_link_types.icon_class IS 'Font Awesome UI icon class';
 COMMENT ON COLUMN project_link_types.is_primary_repository IS 'Is this type of link for the primary source-code repository?';
+COMMENT ON COLUMN project_link_types.created_by_automation IS 'Indicates that the link type can be created by an automation';
 
 GRANT SELECT ON project_link_types TO reader;
 GRANT SELECT, INSERT, UPDATE, DELETE ON project_link_types TO admin;
