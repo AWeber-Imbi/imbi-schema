87f8a85ce8ae32a4d0b581cc60164e3ef574bab1
Dave Shawley <daves@aweber.com>
2022-09-06 09:27:07 -0400

    Rename recorded columns in project notes
    The API framework does not understand `recorded_at` columns without a
    bit of work.  It expects addressable entities to use `created_at`
    instead.

--- tables/v1/project_notes.sql
+++ tables/v1/project_notes.sql
@@ -4,19 +4,19 @@ DROP TABLE IF EXISTS project_notes;
 
 CREATE TABLE project_notes (
     project_id INTEGER,
-    recorded_by TEXT,
-    recorded_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_by TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     content TEXT NOT NULL,
     updated_by TEXT,
     updated_at TIMESTAMP WITH TIME ZONE,
-    PRIMARY KEY (project_id, recorded_at, recorded_by),
+    PRIMARY KEY (project_id, created_at, created_by),
     FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 COMMENT ON TABLE project_notes IS 'Stores notes about a project';
 COMMENT ON COLUMN project_notes.project_id IS 'The project ID';
-COMMENT ON COLUMN project_notes.recorded_by IS 'The user that created the note';
-COMMENT ON COLUMN project_notes.recorded_at IS 'Time that the note was created';
+COMMENT ON COLUMN project_notes.created_by IS 'The user that created the note';
+COMMENT ON COLUMN project_notes.created_at IS 'Time that the note was created';
 COMMENT ON COLUMN project_notes.content IS 'Textual content of the note';
 COMMENT ON COLUMN project_notes.updated_by IS 'The user that last updated the note';
 COMMENT ON COLUMN project_notes.updated_at IS 'Time that the note was last updated or NULL';
