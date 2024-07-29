384fa32d343c9d71317f1ddf1bfbcc5f1451531b
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2024-07-29 16:51:44 -0400

    Add project dependency automation category types

--- types/v1/automation_category_type.sql
+++ types/v1/automation_category_type.sql
@@ -1,6 +1,8 @@
 SET search_path=v1;
 
 CREATE TYPE automation_category_type
-AS ENUM ('create-project');
+AS ENUM ('create-project',
+         'create-project-dependency',
+         'remove-project-dependency');
 
 COMMENT ON TYPE v1.automation_category_type IS 'Identifies the actions that an automation is available for';
