a58168e3ef43258677db89cfa8cd67ce20309b49
Dave Shawley <daves@aweber.com>
2024-10-15 15:33:14 -0400

    Add operations_log.performed_by
    This is a user-input column that is used to override recorded_by for
    display purposes.

--- tables/v1/operations_log.sql
+++ tables/v1/operations_log.sql
@@ -12,17 +12,20 @@ CREATE TABLE IF NOT EXISTS operations_log (
   description   TEXT                      NOT NULL,
   link          TEXT,
   notes         TEXT,
+  performed_by  TEXT,
   ticket_slug   TEXT,
   version       TEXT,
   FOREIGN KEY (project_id) REFERENCES v1.projects (id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY (environment) REFERENCES v1.environments (name) ON UPDATE CASCADE ON DELETE CASCADE
+  FOREIGN KEY (environment) REFERENCES v1.environments (name) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (performed_by) REFERENCES v1.users (username) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 COMMENT ON TABLE operations_log IS 'An audit log of entity operational changes';
 COMMENT ON COLUMN operations_log.id IS 'A surrogate key for modifying and deleting the record';
 COMMENT ON COLUMN operations_log.occurred_at IS 'When the change occurred (user input)';
 COMMENT ON COLUMN operations_log.recorded_at IS 'When the record was created';
-COMMENT ON COLUMN operations_log.recorded_by IS 'The user who recorded the change';
+COMMENT ON COLUMN operations_log.recorded_by IS 'The authenticated user who recorded the change';
+COMMENT ON COLUMN operations_log.performed_by IS 'Optional user to display (user input)';
 COMMENT ON COLUMN operations_log.completed_at IS 'If specified, indicates the change occurred over a span of time';
 COMMENT ON COLUMN operations_log.project_id IS 'The optional ID of the project for the change';
 COMMENT ON COLUMN operations_log.environment IS 'The operational environment the change was made in';
@@ -33,6 +36,7 @@ COMMENT ON COLUMN operations_log.notes IS 'Optional notes for the change in mark
 COMMENT ON COLUMN operations_log.ticket_slug IS 'An optional slug of the ticket that the change was made for';
 COMMENT ON COLUMN operations_log.version IS 'An optional version that the change was made for';
 
+CREATE INDEX ON operations_log (performed_by);
 CREATE INDEX ON operations_log (project_id);
 CREATE INDEX ON operations_log (recorded_at);
 
